054956d643f6f2add99174bb8225426149a51c04
Asmita-Padmanabhan <asmita2310093@ssn.edu.in>
2026-02-07 13:14:33 +0530

    default routing changed

--- linktree-clone/src/App.jsx
+++ linktree-clone/src/App.jsx
@@ -5,13 +5,16 @@ import AdminLogin from './pages/AdminLogin'
 import AdminDashboard from './pages/AdminDashboard'
 
 function App() {
+  // Change 'demo' to your username to make it the default
+  const DEFAULT_USERNAME = 'Musical Musings'
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/:username" element={<PublicView />} />
         <Route path="/:username/admin" element={<AdminLogin />} />
         <Route path="/:username/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/" element={<Navigate to="/demo" replace />} />
+        <Route path="/" element={<Navigate to={`/${DEFAULT_USERNAME}`} replace />} />
       </Routes>
     </BrowserRouter>
   )
